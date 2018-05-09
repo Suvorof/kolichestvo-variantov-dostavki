bc3fcf0025be11028560d1c5e42a823c313739d7
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 18:31:43 +0300

    Добавьте файлы проекта.

---
+++ kolichestvo variantov dostavki/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolichestvo_variantov_dostavki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число клиентов");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+
+            do
+            {
+                result *= x;
+                x -= 1;
+            } while (x > 0);
+            Console.WriteLine("Количество вариантов доставки: " + result);
+            Console.ReadKey();
+        }
+    }
+}
